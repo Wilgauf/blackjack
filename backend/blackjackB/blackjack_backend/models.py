d81196246c20b40660a91fc0e6d48d5214c85d21
JFranklin <franklin.jd2142@gmail.com>
2021-07-29 18:50:20 -0400

    update apps in settings. update models, more progress on logic

--- backend/blackjackB/blackjack_backend/models.py
+++ backend/blackjackB/blackjack_backend/models.py
@@ -2,12 +2,17 @@ from django.db import models
 
 # Create your models here.
 class Game(models.Model):
+    BLACKJACK = (
+        ('P', 'Player'),
+        ('D', 'Dealer'),
+    )
     # player = models.ForeignKey(User, on_delete=models.CASCADE, related_name='games')
     deck_id = models.CharField(max_length=200)
     player_bet = models.IntegerField(6)
     player_hand = models.CharField(max_length=64)
     dealer_hand = models.CharField(max_length=64)
-    player_break = models.BooleanField(default=False)
-    dealer_break = models.BooleanField(default=False)
+    player_bust = models.BooleanField(default=False)
+    dealer_bust = models.BooleanField(default=False)
     player_chips = models.IntegerField(6)
     active = models.BooleanField(default=True)
+    blackjack = models.CharField(max_length=1, choices=BLACKJACK, blank=True)
--- backend/blackjackB/blackjack_backend/utils.py
+++ backend/blackjackB/blackjack_backend/utils.py
@@ -5,7 +5,20 @@ Game Logic helper functions will go here
 """
 
 card_key = {
-    ''
+    'K':10,
+    'Q':10,
+    'J':10,
+    '1':10,
+    'A':1,
+    '2':2,
+    '3':3,
+    '4':4,
+    '5':5,
+    '6':6,
+    '7':7,
+    '8':8,
+    '9':9,
+
 }
 
 #expects 'player_bet' is in request from frontend
@@ -17,6 +30,9 @@ def create_new_game(request):
     player_bet = int(request.data['player_bet'])
     player_chips = 1000 - player_bet
 
+    if check_hand(player_hand) == 'blackjack':
+        blackjack = 'P'
+
     request.data.update({
         'deck_id': deck_id,
         'player_hand': player_hand,
@@ -24,4 +40,21 @@ def create_new_game(request):
         'player_chips': player_chips
     })
     return request
+#parses and computes value of given hand. Will return 'blackjack', 'bust', or the hand value. 
+def check_hand(cards):
+    card_values = []
+    for i, card in enumerate(sorted(cards, reverse=True)):
+        if i == 0 and card[0] == 'A':
+            card_values.append(11)
+        card_values.append(card_key[card[0]])
+    hand_value = sum(card_values)
+    if hand_value > 21:
+        return 'bust'
+    elif hand_value == 21:
+        return 'blackjack'
+    
+    return hand_value
+        
+        
+
 
