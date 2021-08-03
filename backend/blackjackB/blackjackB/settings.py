a35a765a9281e09fd28d8f1befa01ece4f05a7fa
JFranklin <franklin.jd2142@gmail.com>
2021-08-03 08:53:34 -0400

    add heroku settings in settings

--- backend/blackjackB/blackjackB/settings.py
+++ backend/blackjackB/blackjackB/settings.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 import datetime
+import django_heroku
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -140,3 +141,4 @@ STATIC_URL = '/static/'
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 CORS_ALLOWED_ORIGINS = ['http://localhost:3000', 'https://localhost:3000']
+django_heroku.settings(locals())
